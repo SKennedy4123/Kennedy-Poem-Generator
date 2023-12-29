faebc9bed436d2d58b5c23d49a9ebd69763251b8
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2023-12-28 22:25:18 -0500

    Updated user experience

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ function aiPoem(response){
     delay: 20,
     cursor: "",
   });  
+
 }
 
 function generatePoem(event) {
@@ -13,10 +14,14 @@ function generatePoem(event) {
 let apiKey = "87fd924aft47ob31f6228617ba830bdb";
 let userInput = document.querySelector("#user-input");
 let apiContext = `Generate a short poem about ${userInput.value}`;
-let apiPrompt = "You are an expert poem writer. Please create one poem no longer than 4 lines and include a title at the top. Give your response in HTML format that separates each poem text line. Do not display it as one paragraph or run-on sentence. Do not erase or re-do the poem once it has been created. Make the title only 4px larger than the poem font-size. Sign the poem with `~SheCodes AI` at the bottom.";
+let apiPrompt = "You are an expert poem writer. Please create one poem no longer than 4 lines and include a title at the top. Give your response in HTML format that separates each poem text line. Do not display it as one paragraph or run-on sentence. Do not erase or re-do the poem once it has been created. Make the title only 4px larger than the poem font-size. Sign the poem with `~SheCodes AI` at the bottom in color `rgb(118, 1, 118)`.";
 apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
 
 axios.get(apiUrl).then(aiPoem);
+
+  let poemElement = document.querySelector("#new-poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `🤔 Generating your poem about ${userInput.value}...`;
 
 
 }
